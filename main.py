f6a0a3e62cb4f9501e675be6e99e2d88fff3380c
dhruv50ae <linkdp2012@gmail.com>
2022-05-05 15:08:56 +0100

    Created dog class

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from random import sample
 
 
-class Sample():
-    pass
+class Dog():
+    species = "Mammal"
+
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
 
 
-x = Sample()
-print(type(x))
+myDog = Dog("Lab", "Alpha")
+otherDog = Dog("Huskie", "Bravo")
+print(otherDog.breed)
+print(myDog.breed)
